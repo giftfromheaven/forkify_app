86c246ccdaa7965dd28a687a48779850206287fd
Ilya Dykin <dddiii06@yandex.ru>
2022-01-27 18:37:28 +0300

    commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd2ba9cfd-c49e-4e40-816f-10f6b63e4528';
+export const MODAL_CLOSE_SEC = 2.5;
